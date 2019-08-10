6d7cd3e9df9d95e75b8cbbda3f1fc2a3c66f6d97
CodeFreak32 <53270238+CodeFreak32@users.noreply.github.com>
2019-08-10 16:11:31 +0530

    Add files via upload
    Main file that takes the string input and prints the final output.

---
+++ Z-A Main.py
@@ -0,0 +1,63 @@
+import os
+input_string=""
+input_string = input("Enter your string ")
+
+i = 0
+length = len(input_string)
+for i in range(length):
+	if (input_string[i]=='a' or input_string=='A'):
+		os.system('python A.py')	
+	elif (input_string[i]=='b' or input_string=='B'):
+		os.system('python B.py')
+	elif (input_string[i]=='c' or input_string=='C'):
+		os.system('python C.py')
+	elif (input_string[i]=='d' or input_string=='D'):
+		os.system('python D.py')
+	elif (input_string[i]=='e' or input_string=='E'):
+		os.system('python E.py')
+	elif (input_string[i]=='f' or input_string=='F'):
+		os.system('python F.py')
+	elif (input_string[i]=='g' or input_string=='G'):
+		os.system('python G.py')
+	elif (input_string[i]=='h' or input_string=='H'):
+		os.system('python H.py')
+	elif (input_string[i]=='i' or input_string=='I'):
+		os.system('python I.py')
+	elif (input_string[i]=='j' or input_string=='J'):
+		os.system('python J.py')
+	elif (input_string[i]=='k' or input_string=='K'):
+		os.system('python K.py')
+	elif (input_string[i]=='l' or input_string=='L'):
+		os.system('python L.py')
+	elif (input_string[i]=='m' or input_string=='M'):
+		os.system('python M.py')
+	elif (input_string[i]=='n' or input_string=='N'):
+		os.system('python N.py')
+	elif (input_string[i]=='o' or input_string=='O'):
+		os.system('python O.py')
+	elif (input_string[i]=='p' or input_string=='P'):
+		os.system('python P.py')
+	elif (input_string[i]=='q' or input_string=='Q'):
+		os.system('python Q.py')
+	elif (input_string[i]=='r' or input_string=='R'):
+		os.system('python R.py')
+	elif (input_string[i]=='s' or input_string=='S'):
+		os.system('python S.py')
+	elif (input_string[i]=='t' or input_string=='T'):
+		os.system('python T.py')
+	elif (input_string[i]=='u' or input_string=='U'):
+		os.system('python U.py')
+	elif (input_string[i]=='v' or input_string=='V'):
+		os.system('python V.py')
+	elif (input_string[i]=='w' or input_string=='W'):
+		os.system('python W.py')
+	elif (input_string[i]=='x' or input_string=='X'):
+		os.system('python X.py')
+	elif (input_string[i]=='y' or input_string=='Y'):
+		os.system('python Y.py')
+	elif (input_string[i]=='z' or input_string=='Z'):
+		os.system('python Z.py')
+	else:
+		print("    \n\n\n\n\n")	
+
+	i += 1
